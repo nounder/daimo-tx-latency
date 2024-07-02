0335c7a56b05bda9a0e545463cf1e28129e2c2df
Rafael Gutkowski <nounderline@libred.org>
2024-07-02 17:14:31 +0200

    New test run
    > Can we summarize/simplify?
    >
    > * "Broadcasting transaction via RPC takes ~160ms" > what does this mean? Blocks are every 2s, so if a transaction is submitted the average latency (submit -> block timestamp) should be >1000ms.
    > * Eyeballing the table, it looks like the average latency (block timestamp > block seen in RPC) is also about 1s.
    >
    > I'd like to get something that looks like this:
    >
    > Provider	submit>block_ts	block_ts>rpc	total (submit>rpc)
    > Alchemy	1100ms	1300ms	2400 ± 950ms
    > Quicknode	1100ms	1400ms	2500 ± 950ms
    > Chainstack	1100ms	1500ms	2600 ± 950ms

---
+++ providers.sql
@@ -0,0 +1,54 @@
+create temp table events as
+select t::timestamp as t, blocktimestamp::timestamp as blocktimestamp, * from read_json_auto('./out/events.jsonl');
+
+select o, e, s, count(*)
+from events
+group by e, s, o
+order by count(*) desc
+;
+
+create temp table times as
+select
+    newblock.o as origin,
+    newblock.blocknumber as block_number,
+    newblock.t as rpc_time,
+    blockdata.blocktimestamp as block_time,
+    sendtx.n as broadcast_duration,
+    sendtx.t as broadcast_time,
+from events newblock, events blockdata, events sendtx
+where
+    newblock.e = 'NewBlock'
+    and blockdata.e = 'NewBlockData'
+    and newblock.blocknumber = blockdata.blocknumber
+
+    and sendtx.e = 'Time'
+    and sendtx.s = 'eth_sendRawTransaction'
+    and list_contains(blockdata.blocktransactions, sendtx.txhash);
+
+select distinct
+    origin as 'provider',
+
+    (
+        select avg(extract(ms from (t.block_time - t.broadcast_time)))::int || ' ms'
+        from times as t
+        where t.origin = rt.origin
+    ) as 'block_ts>rpc',
+
+    (
+        select avg(extract(ms from (t.rpc_time - t.block_time)))::int || ' ms'
+        from times as t
+        where t.origin = rt.origin
+    ) as 'block_ts>rpc',
+
+    (
+        select
+            avg(extract(ms from (t.rpc_time - t.broadcast_time)))::int
+            || ' ± '
+            || stddev((extract(ms from (t.rpc_time - t.broadcast_time))))::int
+            || ' ms'
+        from times as t
+        where t.origin = rt.origin
+    ) as 'total (submit>rpc)',
+from times rt
+;
+
