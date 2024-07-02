250a85b35213de8039af0af47b747bd7867c2cf6
Rafael Gutkowski <nounderline@libred.org>
2024-07-02 17:14:13 +0200

    Format query with sqlfmt

--- events.sql
+++ events.sql
@@ -1,27 +1,34 @@
 create temp table events as
 select t::timestamp as t, * from read_json_auto('./out/events.jsonl');
 
-select e, s, count(*) from events group by e, s  order by count(*) desc;
+select e, s, count(*)
+from events
+group by e, s
+order by count(*) desc
+;
 
-select 
-  min(n)::decimal(8, 1) as min,
-  max(n)::decimal(8, 1) as min,
-  avg(n)::decimal(8, 1) as avg,
-  stddev_samp(n)::decimal(8, 1) as stddev,
-from events where e = 'Time' and s = 'eth_sendRawTransaction';
+select
+    min(n)::decimal(8, 1) as min,
+    max(n)::decimal(8, 1) as min,
+    avg(n)::decimal(8, 1) as avg,
+    stddev_samp(n)::decimal(8, 1) as stddev,
+from events
+where e = 'Time' and s = 'eth_sendRawTransaction'
+;
 
 
 select
-  newBlock.blockNumber as block_number,
-  newBlock.t as rpc_time,
-  blockData.blockTimestamp as block_time,
-  sendTx.n as broadcast_duration
-from events newBlock, events blockData, events sendTx
+    newblock.blocknumber as block_number,
+    newblock.t as rpc_time,
+    blockdata.blocktimestamp as block_time,
+    sendtx.n as broadcast_duration
+from events newblock, events blockdata, events sendtx
 where
-  newBlock.e = 'NewBlock'
-  and blockData.e = 'NewBlockData' 
-  and newBlock.blockNumber = blockData.blockNumber 
+    newblock.e = 'NewBlock'
+    and blockdata.e = 'NewBlockData'
+    and newblock.blocknumber = blockdata.blocknumber
 
-  and sendTx.e = 'Time' and sendTx.s = 'eth_sendRawTransaction'
-  and list_contains(blockData.blockTransactions, sendTx.txHash)
+    and sendtx.e = 'Time'
+    and sendtx.s = 'eth_sendRawTransaction'
+    and list_contains(blockdata.blocktransactions, sendtx.txhash)
 
